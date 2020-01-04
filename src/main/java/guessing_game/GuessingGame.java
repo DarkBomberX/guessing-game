6ca7428f6f29f69d047ffca8b0431013a1199b98
Edward Mills <EdwardPMills@gmail.com>
2020-01-04 12:07:39 -0500

    Initial commit for Guessing Game

---
+++ src/main/java/guessing_game/GuessingGame.java
@@ -0,0 +1,14 @@
+package guessing_game;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Guess an Number between 1 and 10");
+		
+	}
+
+}

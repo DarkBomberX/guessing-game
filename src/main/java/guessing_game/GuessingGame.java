08e4226b403b50f1631c72e2c9e7dc8f8a7ade40
Edward Mills <EdwardPMills@gmail.com>
2020-01-04 14:21:23 -0500

    Final Guessing Game

--- src/main/java/guessing_game/GuessingGame.java
+++ src/main/java/guessing_game/GuessingGame.java
@@ -4,11 +4,28 @@ import java.util.Scanner;
 
 public class GuessingGame {
 
+	private static final int WINNING_NUMBER = 7;
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Guess an Number between 1 and 10");
-		
-	}
 
+		System.out.println("Guess an Number between 1 and 10");
+
+		int userGuess = input.nextInt();
+
+		for (int guessCount = 0; guessCount < 1; guessCount++) {
+			while (userGuess == 0) {
+				System.out.println("Enter a Value between 1 & 10 and \nif you guess right you will win!");
+				userGuess = input.nextInt();
+
+				if (userGuess != WINNING_NUMBER) {
+					System.out.println("You Lose");
+					userGuess = input.nextInt();
+				} else {
+					System.out.println("You Win!");
+					break;
+				}
+			}
+		}
+	}
 }
